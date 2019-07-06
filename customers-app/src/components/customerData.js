a218172bdf8f149b722a1e5f28a1db12e98cddae
Jesus Yegres <jesusyegrestx@gmail.com>
2019-07-06 14:14:40 -0500

    New app

---
+++ customers-app/src/components/customerData.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const customerData = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+customerData.propTypes = {
+    
+};
+
+export default customerData;
