a2c4eb492d88e1ad51c090d01ffaf2c195002e8b
Jesus Yegres <jesusyegrestx@gmail.com>
2019-07-10 23:23:25 -0500

    minor change

--- customers-app/src/components/customerEdit.js
+++ customers-app/src/components/customerEdit.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const customerEdit = ({name,dni,age}) => {
     return (
         <div>
-            <h2>Edicion Cliente</h2>
+            <h2>Edicion Cliente:</h2>
             <h3>Nombre: {name} / DNI: {dni} / Edad:{age}</h3>
         </div>
     );
